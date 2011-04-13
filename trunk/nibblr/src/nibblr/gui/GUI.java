d789fb5a77e1e8cbb36c1eb134b874dd3e19d5fa
hybris@o2.pl <hybris@o2.pl@b3ac18df-5834-8356-8a1f-7ef7425f8fbe>
2011-04-13 11:32:21 +0000

    commit #2

--- trunk/nibblr/src/nibblr/gui/GUI.java
+++ trunk/nibblr/src/nibblr/gui/GUI.java
@@ -1,18 +1,33 @@
 package nibblr.gui;
 
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
-public class GUI {
+public class GUI implements Runnable {
 	public GUI() {
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void run() {
 		Display display = new Display();
-		final Shell shell = new Shell(display);
+		Shell shell = new Shell(display);
 		shell.setText(Values.APP_NAME);
 		
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 1;
+		layout.marginHeight = 0;
+		layout.marginWidth = 0;
+		
+		shell.setLayout(layout);
+				
 		new MenuBar(shell);
 		new MainToolBar(shell);
+		new MainContainer(shell);
 		
-		shell.open ();
+		shell.open();
 		while(!shell.isDisposed()) {
 			if(!display.readAndDispatch())
 				display.sleep();
--- trunk/nibblr/src/nibblr/gui/Values.java
+++ trunk/nibblr/src/nibblr/gui/Values.java
@@ -18,20 +18,30 @@ public class Values {
 	static final String SUBMENU_IMPORT = "Import";
 	static final String SUBMENU_EXPORT = "Export";
 	static final String SUBMENU_EXIT = "Exit";
-	
 	static final String SUBMENU_SEARCH = "Search";
 	static final String SUBMENU_PREFERENCES = "Preferences";
-	
 	static final String SUBMENU_MAXIMIZE = "Maximize";
 	static final String SUBMENU_MINIMIZE = "Minimize";
 	static final String SUBMENU_RESTORE = "Restore";
-	
 	static final String SUBMENU_ABOUT = "About";
 	static final String SUBMENU_HELP = "Help";
 	
 	static final String TOOLTIP_ADD = "Add";
 	static final String TOOLTIP_SYNCHRONIZE = "Synchronize";
-	static final String TOOLTIP_FILTER = "Filter";
 	static final String TOOLTIP_SEARCH = "Search";
 	static final String TOOLTIP_PREFERENCES = "Preferences";
+	
+	static final String FILTER_ALL = "All";
+	static final String FILTER_READ = "Read";
+	static final String FILTER_UNREAD = "Unread";
+	
+	static final String CHANNELS_MENU_MARK = "Mark as read";
+	static final String CHANNELS_MENU_SYNCHRONIZE = "Synchronize";
+	static final String CHANNELS_MENU_EDIT = "Edit tags";
+	static final String CHANNELS_MENU_DELETE = "Delete";
+	
+	static final String ITEMS_COLUMN_TITLE = "Title";
+	static final String ITEMS_COLUMN_DATE = "Date";
+	
+	static final String ITEMS_MENU_DELETE = "Delete";
 }
